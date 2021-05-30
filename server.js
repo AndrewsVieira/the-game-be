cc6cc69d2d2caeafa8cc6a4c9532ac30331327d5
Schwaaan <jair.schwan9@gmail.com>
2021-05-30 16:58:26 -0300

    create crud alternatives and crud question

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const hostname = "0.0.0.0";
 
 const { login, logout } = require('./src/controllers/loginController.js');
 const auth = require('./src/infra/auth.js');
-const { create, getAll, getById, update, deleteById } = require('./src/controllers/userController')
+const { create, getAll, getById, update, deleteById } = require('./src/controllers/userController');
+const { createAlternative, getAllAlternatives, getAlternativeById, updateAlternative, deleteAlternativeById } = require('./src/controllers/alternativeController')
+const { createQuestion, getAllQuestions, getQuestionById, updateQuestion, deleteQuestionById } = require('./src/controllers/questionController')
 
 app.use(cors());
 app.use(express.json());
@@ -21,6 +23,20 @@ app.get('/v1/users', getAll);
 app.get('/v1/users/:id', getById);
 app.put('/v1/users/:id', update);
 app.delete('/v1/users/:id', deleteById);
+
+// Crud Alternativas
+app.post('/v1/alternatives', createAlternative);
+app.get('/v1/alternatives', getAllAlternatives);
+app.get('/v1/alternatives/:id', getAlternativeById);
+app.put('/v1/alternatives/:id', updateAlternative);
+app.delete('/v1/alternatives/:id', deleteAlternativeById);
+
+// CRUD question
+app.post('/v1/questions', createQuestion);
+app.get('/v1/questions', getAllQuestions);
+app.get('/v1/questions/:id', getQuestionById);
+app.put('/v1/questions/:id', updateQuestion);
+app.delete('/v1/questions/:id', deleteQuestionById);
 
 app.listen(port, hostname, () => {
     console.log(`O servidor está rodando na porta: ${port}`);
---
+++ src/controllers/alternativeController.js
@@ -0,0 +1,77 @@
+const Alternative = require("../models/alternative");
+const Message = require("../utils/message");
+
+exports.createAlternative = (req, res) => {
+    let body = req.body;
+
+    console.log(body);
+
+    if (body.alternative == null || body.id_question == null || body.is_right == null) {
+        return res.status(400).json(Message("Os Dados não podem ser nulos!"));
+    }
+
+    Alternative.create({
+        alternative: body.alternative,
+        id_question: body.id_question,
+        is_right : body.is_right
+    }).then(user => {
+        return res.json(user);
+    }).catch(err => {
+        console.log(err);
+    })
+
+}
+
+exports.getAllAlternatives = (req, res) => {
+    Alternative.findAll().then(user => {
+        return res.json(user);
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
+exports.getAlternativeById = (req, res) => {
+    Alternative.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(user => {
+        return res.json(user);
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
+exports.updateAlternative = (req, res) => {
+
+    let body = req.body;
+
+    if (body.alternative == null || body.isRight == null) {
+        return res.status(400).json(Message("Os Dados não podem ser nulos!"));
+    }
+
+    Alternative.update({
+        alternative: body.alternative,
+        isRight: body.isRight
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(user => {
+        return res.json(user);
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
+exports.deleteAlternativeById = (req, res) => {
+    Alternative.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(alternative => {
+        return res.json(alternative);
+    }).catch(err => {
+        console.log(err)
+    })
+}
