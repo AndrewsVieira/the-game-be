2cffc26627c3b82d73ca6fbaa3e1db98df7ea0f7
AndrewsVieira <andrews.a.vieira@gmail.com>
2021-05-30 14:38:34 -0300

    alterando db_schema

--- db_schema.sql
+++ db_schema.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `the_game` 
+CREATE DATABASE  IF NOT EXISTS `the_game`;
 USE `the_game`;
 
 DROP TABLE IF EXISTS `user`;
@@ -9,3 +9,8 @@ CREATE TABLE `user` (
   `password` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Sempre no fim do arquivo;
+DROP USER IF EXISTS 'pi3fase';
+CREATE USER 'pi3fase'@'%' IDENTIFIED BY 'pi3fase';
+GRANT SELECT, INSERT, UPDATE, DELETE ON the_game.* TO 'pi3fase'@'%';
