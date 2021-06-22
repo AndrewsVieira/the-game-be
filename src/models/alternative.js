582b59e74edeedaf5799bd4b3c18870f463e3b25
alissonfv <61033933+alissonfv@users.noreply.github.com>
2021-06-22 20:59:18 -0300

    Ajuste End Point Quention

--- src/models/alternative.js
+++ src/models/alternative.js
@@ -24,7 +24,7 @@ const Alternative = connParams.define('alternative', {
     timestamps: false
 })
 
-Alternative.hasOne(Question, {
+Alternative.belongsTo(Question, {
     foreignKey: 'id_question'
 });
 
